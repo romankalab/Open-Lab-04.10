2939343141207f1f46f5e06c8308100b2cc2b4d1
romankalab <roan.kalab@gmail.com>
2019-11-07 08:27:47 +0000

    Initial

--- Open-Lab-04.10/Calculator.cs
+++ Open-Lab-04.10/Calculator.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+
 namespace Open_Lab_04._10
 {
     public class Calculator
     {
         public float Average(int[] nums)
         {
-            throw new NotImplementedException();
+            float solution = 0;
+            for (var x = 0; x < nums.Length; x++)
+                solution = solution + nums[x];
+
+            solution = solution / nums.Length;
+            return solution;
         }
     }
 }
